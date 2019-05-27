9800c8cf8dc45dc3e9f821045597983c589cc081
Edson Garcia <ednosmab@yahoo.com.br>
2019-05-26 23:03:59 -0300

    Realizando correcoes no arquivo valida.js, para enviar o valor de forma mais modelada para o PHP

--- valida.js
+++ valida.js
@@ -2,11 +2,17 @@ let alterar = ""
 
 function capturar() {
     alterar = form.grauId.value
+    alterar = alterar.replace(/,/g, '.')
     if (alterar.match(/^\+/g)) {
         alterar = alterar.replace(/^\+/g, '')
     }
-    if (alterar.match(/\,/g)) {
-        alterar = alterar.replace(/,/g, '.')
+    if (alterar.match(/[0-9]{1,2}[\.]/g)) {
+
+    } else {
+        alterar = alterar.replace(/[0-9]{1,2}$/g, `${alterar}.00`)
+    }
+    if (alterar.match(/--/g)) {
+        alterar = alterar.replace(/--/g, '-')
     }
     document.getElementById("grauId").value = alterar
         //window.location.href = 'recebe.php'
